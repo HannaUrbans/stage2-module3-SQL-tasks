e7fd9af8493637c27aae62ea8ce2d08c0123f6a8
Hanna_Urban <huan92@yandex.ru>
2024-04-04 21:56:13 +0300

    corrected foreign keys

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,8 +1,8 @@
-CREATE TABLE student (id BIGINT PRIMARY KEY, name VARCHAR(255), birthday DATE, groupumber INT);
+CREATE TABLE student (id BIGINT PRIMARY KEY, name VARCHAR(255), birthday DATE, groupnumber INT);
 CREATE TABLE subject (id BIGINT PRIMARY KEY, name VARCHAR(255), description VARCHAR(255), grade INT);
 CREATE TABLE paymenttype (id BIGINT PRIMARY KEY, name VARCHAR(255));
-CREATE TABLE payment (id BIGINT PRIMARY KEY, type_id BIGINT FOREIGN KEY REFERENCES paymenttype(id), amount DECIMAL, student_id BIGINT FOREIGN KEY REFERENCES student(id), payment_date DATETIME);
-CREATE TABLE mark (id BIGINT PRIMARY KEY, student_id BIGINT FOREIGN KEY REFERENCES student(id), subject_id BIGINT FOREIGN KEY REFERENCES subject(id), mark INT);
+CREATE TABLE payment (id BIGINT PRIMARY KEY, type_id BIGINT, amount DECIMAL, student_id BIGINT, payment_date DATETIME, FOREIGN KEY (type_id) REFERENCES paymenttype(id), FOREIGN KEY (student_id) REFERENCES student(id));
+CREATE TABLE mark (id BIGINT PRIMARY KEY, student_id BIGINT, subject_id BIGINT, mark INT, FOREIGN KEY (student_id) REFERENCES student(id), FOREIGN KEY (subject_id) REFERENCES subject(id));
 
 
 
