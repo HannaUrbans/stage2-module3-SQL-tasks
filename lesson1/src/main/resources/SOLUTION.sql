5f5e5cfddfb041c20941b9327ea4852ca0cd2544
Hanna_Urban <huan92@yandex.ru>
2024-04-04 22:10:03 +0300

    added autoincrement

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,8 +1,8 @@
-CREATE TABLE student (id BIGINT PRIMARY KEY, name VARCHAR(255), birthday DATE, groupnumber INT);
-CREATE TABLE subject (id BIGINT PRIMARY KEY, name VARCHAR(255), description VARCHAR(255), grade INT);
-CREATE TABLE paymenttype (id BIGINT PRIMARY KEY, name VARCHAR(255));
-CREATE TABLE payment (id BIGINT PRIMARY KEY, type_id BIGINT, amount DECIMAL, student_id BIGINT, payment_date DATETIME, FOREIGN KEY (type_id) REFERENCES paymenttype(id), FOREIGN KEY (student_id) REFERENCES student(id));
-CREATE TABLE mark (id BIGINT PRIMARY KEY, student_id BIGINT, subject_id BIGINT, mark INT, FOREIGN KEY (student_id) REFERENCES student(id), FOREIGN KEY (subject_id) REFERENCES subject(id));
+CREATE TABLE student (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), birthday DATE, groupnumber INT);
+CREATE TABLE subject (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), description VARCHAR(255), grade INT);
+CREATE TABLE paymenttype (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255));
+CREATE TABLE payment (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, type_id BIGINT, amount DECIMAL, student_id BIGINT, payment_date DATETIME, FOREIGN KEY (type_id) REFERENCES paymenttype(id), FOREIGN KEY (student_id) REFERENCES student(id));
+CREATE TABLE mark (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, student_id BIGINT, subject_id BIGINT, mark INT, FOREIGN KEY (student_id) REFERENCES student(id), FOREIGN KEY (subject_id) REFERENCES subject(id));
 
 
 
