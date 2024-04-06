9123e61ed4b54ea929eda54934f1ae0aa3c095a6
Hanna_Urban <huan92@yandex.ru>
2024-04-06 14:55:25 +0300

    corrected db

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,8 +1,8 @@
 SELECT * FROM payment WHERE amount >=500;
-SELECT * FROM student WHERE birthday <= DATEADD ('YEAR', -20, '2024-04-04');
-SELECT * FROM student WHERE birthday >= DATEADD ('YEAR', -20, '2024-04-04') AND groupnumber = 10;
+SELECT * FROM student WHERE birthday <= DATEADD ('YEAR', -20, CURRENT_DATE);
+SELECT * FROM student WHERE birthday >= DATEADD ('YEAR', -20, CURRENT_DATE) AND groupnumber = 10;
 SELECT * FROM student WHERE name = 'Mike' OR groupnumber = 4 OR groupnumber = 5 OR groupnumber = 6;
-SELECT * FROM payment WHERE payment_date >= DATEADD('MONTH', -8, '2024-04-04');
+SELECT * FROM payment WHERE payment_date >= DATEADD('MONTH', -8, CURRENT_DATE);
 SELECT * FROM student WHERE name LIKE 'A%';
 SELECT * FROM student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
 
--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,3 +1,3 @@
 SELECT name FROM student WHERE (SELECT AVG(mark) FROM mark) > 8;
 SELECT id, name FROM student WHERE (SELECT MIN(mark) FROM mark) > 7;
-SELECT id, name FROM student WHERE (SELECT COUNT (*) FROM payment WHERE student.id = payment.student_id AND payment_date >= '2019-01-01' AND payment_date <= '2019-12-31') >=2;
+SELECT student.id, student.name FROM student student INNER JOIN payment payment ON student.id = payment.student_id WHERE payment.payment_date >= '2019-01-01' AND payment.payment_date <= '2019-12-31' GROUP BY student.id, student.name HAVING COUNT(*) >= 2;
