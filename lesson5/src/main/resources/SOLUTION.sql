cf158b1c7ad0d5b394c6390b828610744f6727df
Hanna_Urban <huan92@yandex.ru>
2024-04-07 01:05:55 +0300

    recorrected dateadd

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,8 +1,8 @@
 SELECT * FROM payment WHERE amount >=500;
-SELECT * FROM student WHERE birthday <= DATEADD ('YEAR', -20, '20001010');
-SELECT * FROM student WHERE birthday >= DATEADD ('YEAR', -20, '20001010') AND groupnumber = 10;
+SELECT * FROM student WHERE birthday <= DATEADD ('YEAR', -20, CURRENT_DATE);
+SELECT * FROM student WHERE birthday >= DATEADD ('YEAR', -20, CURRENT_DATE) AND groupnumber = 10;
 SELECT * FROM student WHERE name = 'Mike' OR groupnumber = 4 OR groupnumber = 5 OR groupnumber = 6;
-SELECT * FROM payment WHERE payment_date >= DATEADD('MONTH', -8, '20001010');
+SELECT * FROM payment WHERE payment_date >= DATEADD('MONTH', -8, CURRENT_DATE);
 SELECT * FROM student WHERE name LIKE 'A%';
 SELECT * FROM STUDENT WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
 
